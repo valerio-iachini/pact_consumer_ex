b9ff39cbd540842855722bf52e2e1f7bc01fe88a
Valerio Iachini <valerio.iachini@gmail.com>
2025-06-19 09:00:48 +0200

    restore version from mix

--- lib/native/pact_consumer.ex
+++ lib/native/pact_consumer.ex
@@ -1,14 +1,14 @@
 defmodule Pact.Native.PactConsumer do
   @moduledoc false
-  @version "0.2.1"
+  version = Mix.Project.config()[:version]
 
   use RustlerPrecompiled,
     otp_app: :pact_consumer_ex,
     crate: "pact_consumer_nif",
     base_url:
-      "https://github.com/valerio-iachini/pact_consumer_ex/releases/download/v#{@version}",
+      "https://github.com/valerio-iachini/pact_consumer_ex/releases/download/v#{version}",
     force_build: System.get_env("PACT_CONSUMER_EX_FORCE_BUILD") == "true",
-    version: @version
+    version: version
 
   use Pact.Native.Builders.InteractionBuilder
   use Pact.Native.Builders.MessageBuilder

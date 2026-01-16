c07503f9a2e24e768a4b9c06cb287671c25885f9
Valerio <valerio.iachini@prima.it>
2026-01-16 19:15:00 +0100

    update checksums

--- checksum-Elixir.Pact.Native.PactConsumer.exs
+++ checksum-Elixir.Pact.Native.PactConsumer.exs
@@ -1,10 +1,10 @@
 %{
-  "libpact_consumer_nif-v0.2.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:7aa0a47d64b2e460c2d62ec37dca931dc94515a8b66937c3dfde447dc89bd66b",
-  "libpact_consumer_nif-v0.2.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:780740d70871f8b039725de7488d68aaedaff2a6920e154a4ac716b2c69fe147",
-  "libpact_consumer_nif-v0.2.2-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:2e9861264b6e08fb49cd4ee9b6b25ed55bfdfd0ee6f2033804e3f696b71b88cc",
-  "libpact_consumer_nif-v0.2.2-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:87951a6f769e33fb41cd38d8dbc6d237b586ea97d717368d2b946af44b14bd5f",
-  "libpact_consumer_nif-v0.2.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:41a781cec0722cbf4954786ba4819d94ea43ff93b754fbb798a9b186eafd1e20",
-  "libpact_consumer_nif-v0.2.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c5d269dc7486820092f4eaf900c70cced1f602903b2dae44db11e813a3931283",
-  "pact_consumer_nif-v0.2.2-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:8a763ca1f8d6345192ec067a675fad9ea954c8b28fe924e51718de82c3f72bf8",
-  "pact_consumer_nif-v0.2.2-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:ef82d87f3459548dc97cb299f2bdca1f35035531e782dcad9a1848e5b19964bc",
+  "libpact_consumer_nif-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:adf56625957f475d875d4c49b2bf9f24bb886aba8a8913d848dd002020327df2",
+  "libpact_consumer_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:a6847e91fd96fb099217b1b0d41111492f6c925698491ff5b1a71bb816bed570",
+  "libpact_consumer_nif-v0.3.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:fe9b87e8a9642b8edf902f5d09f7c556567d11f5b98aa0b7c65c939e50b985c7",
+  "libpact_consumer_nif-v0.3.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:98a52a39fce7823c03d9c11e7e6ac51e8b0cc64e65d7779495e064d367cf5a98",
+  "libpact_consumer_nif-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:41f25b9129f5e93a7fcf6b57a83025691268300f0eb7f6732ae0ca071ed8f6ee",
+  "libpact_consumer_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:e491b802ad6f5a04c704a9324b29c79b493a3e5eef352c01a1f567a13b5201f5",
+  "pact_consumer_nif-v0.3.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:7bc4148957ce5e8f31a763ecd68f2ee034229f9d23f8090f60595e9eafb9eab9",
+  "pact_consumer_nif-v0.3.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:da87b1ac9d76880cd1ea9c028d13f00221abfd8626da7f68a3ba5b5b06def9d7",
 }
